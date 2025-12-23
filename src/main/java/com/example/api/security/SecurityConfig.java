3b4f8fe934c9b9fa87f1267f883f5f327dcad064
therdean <dejanristevski96@gmail.com>
2025-12-23 03:29:26 +0100

    redundant web config removal / changed to allow * on api

--- src/main/java/com/example/api/security/SecurityConfig.java
+++ src/main/java/com/example/api/security/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://132.164.178.164"));
+        configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
