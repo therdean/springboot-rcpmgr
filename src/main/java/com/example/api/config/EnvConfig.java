632be86708282e47c09f946b26c9cc35c9c72102
therdean <dejanristevski96@gmail.com>
2025-12-23 03:21:15 +0100

    updates

--- src/main/java/com/example/api/config/EnvConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.api.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-@PropertySource("file:.env")
-public class EnvConfig {
-}
--- src/main/java/com/example/api/repository/RecipeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.api.repository;
-
-import com.example.api.entity.Recipe;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecipeRepository extends JpaRepository<Recipe, Long> {
-}
